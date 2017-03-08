2f21b07a693b0f3eabf826d4807e613be0f0f940
Esteban Pastorino <ejpastorino@gmail.com>
2017-03-08 11:38:17 +0100

    with sustained keypresses

--- Main.elm
+++ Main.elm
@@ -2,24 +2,35 @@ import Color exposing (..)
 import Collage exposing (..)
 import Element exposing (..)
 import Keyboard
+import Keyboard.Extra
+import Time exposing (Time, second)
 import Html exposing (..)
 
 type alias Model =
   { points : List Point
   , x : Int
   , y : Int
+  , keyboardState : Keyboard.Extra.State
   }
 
 type alias Point = (Int, Int)
 
-type Msg = KeyUp Keyboard.KeyCode
+type Msg
+  = KeyboardExtraMsg Keyboard.Extra.Msg
+  | Tick Time
 
-initialModel : Model
+initialModel : ( Model, Cmd Msg )
 initialModel =
-  { points = [(0, 0)]
-  , x = 0
-  , y = 0
-  }
+  let
+      keyboardState = Keyboard.Extra.initialState
+  in
+    ( { points = [(0, 0)]
+      , x = 0
+      , y = 0
+      , keyboardState= keyboardState
+      }
+    , Cmd.none
+    )
 
 view : Model -> Html Msg
 view model =
@@ -42,31 +53,35 @@ drawLine points =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Keyboard.ups KeyUp
+  Sub.batch
+  [ Sub.map KeyboardExtraMsg Keyboard.Extra.subscriptions
+  , Time.every (1/30 * second) Tick
+  ]
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    KeyUp keyCode ->
-      (keyUp keyCode model, Cmd.none)
-
-keyUp : Keyboard.KeyCode -> Model -> Model
-keyUp keyCode model =
-  case keyCode of
-    38 -> -- up
-      { model | y = model.y + 1, points = (model.x, model.y + 1) :: model.points }
-    40 -> -- down
-      { model | y = model.y - 1, points = (model.x, model.y - 1) :: model.points }
-    39 -> -- right
-      { model | x = model.x + 1, points = (model.x + 1, model.y) :: model.points }
-    37 -> -- left
-      { model | x = model.x - 1, points = (model.x - 1, model.y) :: model.points }
-    _ -> model
+    KeyboardExtraMsg keyMsg ->
+      let
+        keyboardState = Keyboard.Extra.update keyMsg model.keyboardState
+      in
+       { model | keyboardState = keyboardState } ! []
+    Tick _ ->
+      let
+        { x, y } = Keyboard.Extra.arrows model.keyboardState
+        newX = model.x + x
+        newY = model.y + y
+      in
+        case (x, y) of
+          (0, 0) ->
+            model ! []
+          _ ->
+            { model | x = newX, y = newY, points = (newX, newY) :: model.points } ! []
 
 main : Program Never Model Msg
 main =
   Html.program
-    { init = initialModel ! []
+    { init = initialModel
     , update = update
     , view = view
     , subscriptions = subscriptions
