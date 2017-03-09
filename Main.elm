b15d35c65b9669c07b3c851dc2af4a7b61a46c40
Esteban Pastorino <ejpastorino@gmail.com>
2017-03-09 11:20:41 +0100

    shake button and animation

--- Main.elm
+++ Main.elm
@@ -4,13 +4,18 @@ import Element exposing (..)
 import Keyboard
 import Keyboard.Extra
 import Time exposing (Time, second)
+import AnimationFrame
+import Animation exposing (..)
 import Html exposing (..)
+import Html.Events exposing (onClick)
 
 type alias Model =
   { points : List Point
   , x : Int
   , y : Int
   , keyboardState : Keyboard.Extra.State
+  , clock : Time
+  , animation : Animation
   }
 
 type alias Point = (Int, Int)
@@ -18,6 +23,7 @@ type alias Point = (Int, Int)
 type Msg
   = KeyboardExtraMsg Keyboard.Extra.Msg
   | Tick Time
+  | Shake
 
 initialModel : ( Model, Cmd Msg )
 initialModel =
@@ -27,16 +33,24 @@ initialModel =
     ( { points = [(0, 0)]
       , x = 0
       , y = 0
-      , keyboardState= keyboardState
+      , keyboardState = keyboardState
+      , clock = 0
+      , animation = static 0
       }
     , Cmd.none
     )
 
 view : Model -> Html Msg
 view model =
-  collage 800 600
-    [ drawLine model.points ]
-    |> Element.toHtml
+  let
+      angle = animate model.clock model.animation
+  in
+    div []
+      [ collage 800 600
+          [ rotate (degrees angle) (drawLine model.points) ]
+          |> Element.toHtml
+      , shakeButton
+      ]
 
 drawLine : List Point -> Form
 drawLine points =
@@ -50,12 +64,23 @@ drawLine points =
     shape
     |> traced (solid red)
 
+shakeButton : Html Msg
+shakeButton =
+  Html.button [ onClick Shake ] [ Html.text "Shake it good!" ]
+
+shakeAnimation : Time -> Animation
+shakeAnimation t =
+  animation t
+    |> from 0
+    |> to 360
+    |> duration (500 * Time.millisecond)
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.batch
   [ Sub.map KeyboardExtraMsg Keyboard.Extra.subscriptions
-  , Time.every (1/30 * second) Tick
+  , AnimationFrame.diffs Tick
   ]
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -66,17 +91,42 @@ update msg model =
         keyboardState = Keyboard.Extra.update keyMsg model.keyboardState
       in
        { model | keyboardState = keyboardState } ! []
-    Tick _ ->
+    Tick dt ->
       let
         { x, y } = Keyboard.Extra.arrows model.keyboardState
         newX = model.x + x
         newY = model.y + y
+        newClock = model.clock + dt
+        (newPoints, newAnimation) =
+          case (model.animation == (static 0)) of
+            True ->
+              (model.points, model.animation)
+            False ->
+              case (isDone model.clock model.animation) of
+                True -> ([], (static 0))
+                False -> (model.points, model.animation)
+
+        newPoints_ =
+          case (x, y) of
+            (0, 0) -> newPoints
+            _ -> (newX, newY) :: newPoints
+
+        model_ =
+          { model
+          | points = newPoints_
+          , clock = newClock
+          , animation = newAnimation
+          }
       in
         case (x, y) of
           (0, 0) ->
-            model ! []
+            model_ ! []
           _ ->
-            { model | x = newX, y = newY, points = (newX, newY) :: model.points } ! []
+            { model_ | x = newX, y = newY } ! []
+    Shake ->
+      { model
+      | animation = shakeAnimation model.clock
+      } ! []
 
 main : Program Never Model Msg
 main =
